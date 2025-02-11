50ec7f0d2eeb71d09b54706bfe0864a6094c770f
RaduLucianR <radlucradulescu@gmail.com>
2025-02-11 15:07:17 +0100

    results of Jiang for Sobhani input max 5k jobs

--- jiang_et_al/generate_Figure9Sobhani.py
+++ jiang_et_al/generate_Figure9Sobhani.py
@@ -8,14 +8,16 @@ def sobhani_figure9():
     results = []
 
     for U in values:
-        path = fr"C:\Users\20191039\Desktop\repos\sag-ros-experiments\sobhani_et_al\tasksets_util_{U}.txt"
+        path = fr"/home/radu/repos/sag-ros-experiments/data/SobhaniExp/Fig9/tasksets_nrofjobs_max_5k/tasksets_util_{U}.txt"
         tasksets = convert_sobhani_synthetic_to_jiang(5, 10, path)
         r = jiang_on_tasksets(tasksets, 4)[0]
         print(f"U={U}, schedulability={r}")
         results.append((U, r))
 
-    with open("Figure9_data.csv", "w+", newline="") as f:
+    with open("Figure9_data_Jiang.csv", "w+", newline="") as f:
         writer = csv.writer(f)
 
         for elem in results:
             writer.writerow([elem[0], elem[1]])
+
+sobhani_figure9()
